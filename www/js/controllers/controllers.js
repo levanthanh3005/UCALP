c2c3825c3ba0ac92f352b1d0da6bedb3288eb35b
feal93 <feal93@gmail.com>
2017-07-14 18:01:54 +0200

    Fix the header_wand.png

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -1029,6 +1029,55 @@ angular.module('leth.controllers', [])
   $scope.$on('modalIMG.shown', function() {
     //console.log('Modal is shown!');
   });
+  $scope.copySeed = function(randomSeed){
+    console.log("Copy Seed");
+    try{
+      let t = document.createElement('textarea')
+      t.id = 't'
+      // Optional step to make less noise in the page, if any!
+      t.style.height = 0;
+      // You have to append it to your page somewhere, I chose <body>
+      document.body.appendChild(t);
+      // Copy whatever is in your div to our new textarea
+      t.value = randomSeed;
+      // Now copy whatever inside the textarea to clipboard
+      let selector = document.querySelector('#t');
+      selector.select();
+      document.execCommand('copy');
+      // Remove the textarea
+      document.body.removeChild(t);
+      var alertPopup = $ionicPopup.alert({
+         title: 'Copy your seed',
+         template: 'Your seed is in clipboard!'
+       });
+
+       alertPopup.then(function(res) {
+         console.log('Your seed copied in clipboard');
+       });
+    } catch (e) {
+      console.log("Error when copy");
+    }
+
+    document.addEventListener("deviceready", function () {
+      $cordovaClipboard
+      .copy(randomSeed)
+      .then(function () {
+        // success
+        //alert('Address in clipboard');
+        var alertPopup = $ionicPopup.alert({
+           title: 'Copy your seed',
+           template: 'Your seed is in clipboard!'
+         });
+
+         alertPopup.then(function(res) {
+           console.log('Your seed copied in clipboard');
+         });
+      }, function () {
+        // error
+        console.log('Copy error');
+      });
+    }, false);
+  }
 
   $scope.goLogin = function(random){
     closeEntropyModal();
